d6ea05af4b7b793f58176abcc21c0dac57b7d30a
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-09-15 23:13:23 -0400

    Created my program structure and disabled nullables warnings

---
+++ P6_QuizMaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P6_QuizMaker // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ P6_QuizMaker/UI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P6_QuizMaker
+{
+    internal class UI
+    {
+    }
+}
