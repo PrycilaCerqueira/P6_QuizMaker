de22b13e1c2e268127e268041c907adffb598f1b
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-09-17 13:34:56 -0400

    Updated the funtions with their XML comments

--- P6_QuizMaker/UI.cs
+++ P6_QuizMaker/UI.cs
@@ -8,6 +8,11 @@ namespace P6_QuizMaker
 {
     internal class UI
     {
+        /// <summary>
+        /// Prints the art game title and the game description on the console for the players
+        /// </summary>
+        /// <param name="gameTitle">Name of the game<param>
+        /// <param name="gameDescription">Description of the game</param>
         public static void PrintGameInstructions(string gameTitle, string gameDescription)
         {
             Console.WriteLine($"Welcome to {gameTitle}");
@@ -15,6 +20,11 @@ namespace P6_QuizMaker
             Console.WriteLine(); //skip a line
         }
 
+        /// <summary>
+        /// Prints the player chosen name and their initial score
+        /// </summary>
+        /// <param name="score">By default the initial score is zero</param>
+        /// <returns>The player chosen name</returns>
         public static string PrintPlayerInfo(int score)
         {
             string inGameID;
