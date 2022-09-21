1c8543922bc5b363c09cf8f04a79a3b07b0a22a8
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-09-20 22:36:03 -0400

    Created a functional FOR loop to increment the quizBank list

--- P6_QuizMaker/Program.cs
+++ P6_QuizMaker/Program.cs
@@ -15,6 +15,7 @@ namespace P6_QuizMaker // Note: actual namespace depends on the project name.
             inGameID.Name = UI.PrintPlayerInfo(inGameID.score);
 
             //TODO: Fix the increment of the list
+            /*
             var quizBank = new List<Quiz>()
             {
                 new Quiz()
@@ -49,8 +50,27 @@ namespace P6_QuizMaker // Note: actual namespace depends on the project name.
                 },
 
             };
+            */
 
-            foreach(var quizItem in quizBank)
+            var quizBank = new List<Quiz>(); //created a variable quizBank to be my LIST DB
+
+            for (int numOfQuestions = 0; numOfQuestions < 2; numOfQuestions++) //Loop to add the which question&answers sub-list to main LIST DB
+            {
+                quizBank.Add(new Quiz // quizBank.add tells the system to add the new instance of Quiz (question&answers sub-list) to the LIST DB
+                {
+                    //Each list element will be input by the user
+                    Topic = UI.TopicInput(), 
+                    Question = UI.QuestionInput(),
+                    Answer1 = UI.AnswersInput(),
+                    Answer2 = UI.AnswersInput(),
+                    Answer3 = UI.AnswersInput(),
+                    Answer4 = UI.AnswersInput(),
+                    CorrectAnswer = UI.RightAnswerInput()
+
+                });
+            }
+            
+            foreach (var quizItem in quizBank)
             {
                 Console.WriteLine();
                 Console.WriteLine(quizItem.Topic);
