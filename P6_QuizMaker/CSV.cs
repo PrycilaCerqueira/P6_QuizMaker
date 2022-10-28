5ad84fac934a9f956f80379de63a2aa58b113d88
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-10-27 22:23:36 -0400

    Added a TODO comment

--- P6_QuizMaker/CSV.cs
+++ P6_QuizMaker/CSV.cs
@@ -8,6 +8,7 @@ namespace P6_QuizMaker
 {
     internal class CSV
     {
+        //TODO: Study the MS links and fix the Answers inner list
         //https://www.youtube.com/watch?v=Ch6dDFuKCV4
         //https://learn.microsoft.com/en-us/dotnet/api/system.object.gettype?view=net-7.0
         //https://learn.microsoft.com/en-us/dotnet/api/system.reflection.propertyinfo.getvalue?view=net-7.0
