bd4ce134a0c22c6d0956a54b60bc9f77b39462cc
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-11-01 21:05:57 -0400

    Updated the comments

--- P6_QuizMaker/XML.cs
+++ P6_QuizMaker/XML.cs
@@ -11,10 +11,14 @@ namespace P6_QuizMaker
 {
     internal class XML
     {
+        /// <summary>
+        /// Converts the QuizCards into a plain text and exports the data locally
+        /// </summary>
+        /// <param name="quizDB"></param>
         public static void ExportFile(List<Quiz> quizDB)
         {
-            var serializer = new XmlSerializer(typeof(List<Quiz>));
-            using (var writer = new StreamWriter(@"C:\Users\pry_p\source\repos\P6_QuizMaker\QuizCards.xml"))
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Quiz>));
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\pry_p\source\repos\P6_QuizMaker\QuizCards.xml"))
             {
                 serializer.Serialize(writer, quizDB);
             }
